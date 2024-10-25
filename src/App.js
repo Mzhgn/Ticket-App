e41bfa702827befb4706c84ec04b79cef488ef71
Mozhgan <mojganshah@gmail.com>
2024-10-25 22:22:00 +0330

    refactor the codes

--- src/App.js
+++ src/App.js
@@ -1,12 +1,10 @@
 import React, { Component } from "react";
 import Ticket from "./components/Ticket/Ticket";
 
-export default class App extends Component {
-  render() {
-    return (
-      <div>
-        <Ticket />
-      </div>
-    );
-  }
+export default function App() {
+  return (
+    <div>
+      <Ticket />
+    </div>
+  );
 }
